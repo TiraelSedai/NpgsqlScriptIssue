b4ebff5ed115d43f5e3db196dfa2a0c49ed726ca
Evgenii Burmistrov <evgenii.burmistrov@abbyy.com>
2021-10-29 21:35:18 +0300

    first commit

---
+++ Migrations/20211029182036_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace EFGetStarted.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Url = table.Column<string>(type: "text", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    WithoutDefault = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Blogs",
+                columns: new[] { "Id", "Timestamp", "Url", "WithoutDefault" },
+                values: new object[] { -1L, new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "https://myblog.ru/intro", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs => Set<Blog>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseNpgsql("Server=localhost;Database=test;Port=5432;User Id=postgres;Password=postgres;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                        .HasData(new Blog
+                        {
+                            Id = -1,
+                            Url = "https://myblog.ru/intro",
+                            Timestamp = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                        });
+        }
+
+        public class Blog
+        {
+            public long Id { get; set; }
+            public string Url { get; set; } = string.Empty;
+            public DateTime Timestamp { get; set; } = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            public DateTime WithoutDefault { get; set; }
+        }
+    }
+}
